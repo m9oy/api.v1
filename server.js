c6d6a0761308d6fff2a566da5f3f627eb4277c9f
m9oy <mhmdqlan073@gmail.com>
2026-05-01 22:21:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const rateLimit = require("express-rate-limit");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // ═══════════════════════════════════════════
 //  Middleware
